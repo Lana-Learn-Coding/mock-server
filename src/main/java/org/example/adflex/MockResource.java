644527cb1eb019aa3b37c584403c74289b8a30fd
lana <sitdownrightnow.dev@gmail.com>
2020-08-10 13:13:09 +0700

    initial

---
+++ src/main/java/org/example/adflex/MockResource.java
@@ -0,0 +1,58 @@
+package org.example.adflex;
+
+import io.vertx.core.json.JsonObject;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/api")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+public class MockResource {
+    private static final String BODY = "body";
+    private static final String HTTP_STATUS_CODE = "http_status_code";
+
+    private final JsonObject config = new JsonObject();
+
+    @POST
+    @Path("/post")
+    public Response apiPost() {
+        int status = config.getInteger(HTTP_STATUS_CODE, 200);
+        if (config.getValue(BODY) instanceof String) {
+            return Response.status(status)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN)
+                .entity(config.getString(BODY))
+                .build();
+        }
+
+        if (config.getValue(BODY) instanceof JsonObject) {
+            return Response.status(status)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .entity(config.getJsonObject(BODY))
+                .build();
+        }
+
+        return Response.status(status)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN)
+            .build();
+    }
+
+    @POST
+    @Path("/config")
+    public Response config(JsonObject config) {
+        if (config.getValue(HTTP_STATUS_CODE) instanceof Integer) {
+            this.config.put(HTTP_STATUS_CODE, config.getInteger(HTTP_STATUS_CODE));
+        }
+
+        if (config.getValue(BODY) instanceof String || config.getValue(BODY) instanceof JsonObject) {
+            this.config.put(BODY, config.getValue(BODY));
+        }
+
+        return Response.ok().build();
+    }
+}
---
+++ src/test/java/org/example/adflex/MockResourceTest.java
@@ -0,0 +1,71 @@
+package org.example.adflex;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import io.vertx.core.json.JsonObject;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class MockResourceTest {
+
+    @Test
+    public void apiPostDefault() {
+        given()
+            .when()
+            .contentType(ContentType.JSON)
+            .post("/api/post")
+            .then()
+            .statusCode(200);
+    }
+
+    @Test
+    public void apiPostJson() {
+        JsonObject config = new JsonObject()
+            .put("http_status_code", 404)
+            .put("body", new JsonObject().put("name", "lana").put("age", 18));
+
+        given()
+            .when()
+            .body(config.toString())
+            .contentType(ContentType.JSON)
+            .post("/api/config")
+            .then()
+            .statusCode(200);
+
+        given()
+            .when()
+            .contentType(ContentType.JSON)
+            .post("/api/post")
+            .then()
+            .statusCode(404)
+            .body("name", equalTo("lana"),
+                "age", equalTo(18));
+    }
+
+    @Test
+    public void apiPostPlainText() {
+        JsonObject config = new JsonObject()
+            .put("http_status_code", 201)
+            .put("body", "Hi, my name is lana");
+
+        given()
+            .when()
+            .contentType(ContentType.JSON)
+            .body(config.toString())
+            .post("/api/config")
+            .then()
+            .statusCode(200);
+
+        given()
+            .when()
+            .contentType(ContentType.JSON)
+            .post("/api/post")
+            .then()
+            .statusCode(201)
+            .body(is("Hi, my name is lana"));
+    }
+}
